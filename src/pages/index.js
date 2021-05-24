de09cbb0fe4fbb6d99b52b936348a30c3c82ee07
fadizahhar <fadi.zahhar@ogilvy.com>
2021-05-25 01:25:32 +0300

    fixing the slice and putting the hero page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,41 @@
 import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+import { graphql } from "gatsby"
+import SliceZone from '../components/slicezone'
 
 import Layout from "../components/layout"
-import Seo from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
-  </Layout>
-)
+
+export const query = graphql`
+{
+  prismic {
+    allHomepages {
+      edges {
+        node {
+          body {
+            ... on PRISMIC_HomepageBodyHero {
+              type
+              label
+              primary {
+                hero_content
+                hero_title
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+`
+
+const IndexPage = (props) => {
+  console.log(props);
+  return(
+    <Layout>
+      <SliceZone body={props.data.prismic.allHomepages.edges[0].node.body} />
+    </Layout>
+  )
+}
 
 export default IndexPage
